2767bf1b1c7b3f038f07e3e02ec3a2b931386cf9
Syazwana <22FTT1515@student.pb.edu.bn>
2024-03-11 23:16:43 +0800

    added route for tabs

--- places/src/app/app.routes.ts
+++ places/src/app/app.routes.ts
@@ -1,29 +1,51 @@
 import { Routes } from '@angular/router';
+import { TabPage } from './tab/tab.page';
 
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: 'places',
+    redirectTo: 'auth',
     pathMatch: 'full',
   },
   {
-    path: 'places',
-    loadComponent: () => import('./places/places.page').then( m => m.PlacesPage)
-  },
-  {
-    path: 'offers',
-    loadComponent: () => import('./offers/offers.page').then( m => m.OffersPage)
-  },
-  {
-    path: 'tab',
-    loadComponent: () => import('./tab/tab.page').then( m => m.TabPage)
-  },
-  {
     path: 'auth',
-    loadComponent: () => import('./auth/auth.page').then( m => m.AuthPage)
+    loadComponent: () => import('./auth/auth.page').then((m) => m.AuthPage),
   },
   {
     path: 'place-detail',
-    loadComponent: () => import('./places/place-detail/place-detail.page').then( m => m.PlaceDetailPage)
+    loadComponent: () =>
+      import('./places/place-detail/place-detail.page').then(
+        (m) => m.PlaceDetailPage
+      ),
+  },
+  {
+    path: 'offers',
+    loadComponent: () =>
+      import('./offers/offers.page').then((m) => m.OffersPage),
+  },
+  {
+    path: 'tabs',
+    component: TabPage,
+    children: [
+      {
+        path: 'places',
+        loadComponent: () =>
+          import('./places/places.page').then((m) => m.PlacesPage),
+      },
+      {
+        path: 'offers',
+        loadComponent: () =>
+          import('./offers/offers.page').then((m) => m.OffersPage),
+      },
+    ],
+  },
+  {
+    path: 'places',
+    redirectTo: '/tabs/places',
+    pathMatch: 'full',
+  },
+  {
+    path: 'tabs',
+    loadComponent: () => import('./tab/tab.page').then((m) => m.TabPage),
   },
 ];
