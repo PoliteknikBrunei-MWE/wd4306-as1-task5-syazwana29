4cd5ae3e8d032a09fb00f61cdc84fafc1b61fba6
Syazwana <22FTT1515@student.pb.edu.bn>
2024-03-11 22:48:50 +0800

    imported cart

--- places/src/app/places/place-detail/place-detail.page.ts
+++ places/src/app/places/place-detail/place-detail.page.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { addIcons } from 'ionicons';
+import { cart } from 'ionicons/icons';
 
 @Component({
   selector: 'app-place-detail',
